e00e9bf77c006a0b5416b2e04d4489a82a07c49c
AleksoBre <aleksstevancevic@gmail.com>
2026-03-16 12:06:00 +0100

    changed lazy loading to eager loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,14 +1,14 @@
 <x-layout>
     <x-slot:heading>About</x-slot>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                    <strong>{{$job['title']}}</strong> pays <strong>{{ $job['salary'] }}</strong>
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                    <div class="font-bold text-blue-500 text-sm ">{{$job->employer->name}}</div>
+                    <div>
+                        <strong>{{$job['title']}}</strong> pays <strong>{{ $job['salary'] }}</strong>
+                    </div>
                 </a>
-            </li>
-            <br>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
