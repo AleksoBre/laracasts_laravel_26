bcdbf369d55bf7766b87f5b5e2dccd3f7e8e3585
AleksoBre <aleksstevancevic@gmail.com>
2026-03-20 13:17:08 +0100

    implemented error showing for validation

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -9,29 +9,44 @@
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-4">
-          <label for="job_title" class="block text-sm/6 font-medium text-gray-900">Job Title</label>
+          <label for="title" class="block text-sm/6 font-medium text-gray-900">Job Title</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="job_title" type="text" name="job_title" placeholder="leatherworker" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+              <input id="title" type="text" name="title" placeholder="leatherworker" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
             </div>
+            @error('title')
+              <p class="mt-2 text-red-500 italic font-semibold">{{ $message }}</p>
+            @enderror
           </div>
         </div>
 
         <div class="sm:col-span-4">
-          <label for="job_salary" class="block text-sm/6 font-medium text-gray-900">Job Salary</label>
+          <label for="salary" class="block text-sm/6 font-medium text-gray-900">Job Salary</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="job_salary" type="text" name="job_salary" placeholder="$100,000 per year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+              <input id="salary" type="text" name="salary" placeholder="$100,000 per year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
             </div>
+            @error('salary')
+              <p class="mt-2 text-red-500 italic font-semibold">{{ $message }}</p>
+            @enderror
           </div>
         </div>
-
+  
       </div>
+      
+      {{-- <div class="mt-10">
+        @if ($errors->any())
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li class="text-red-500 italic">{{$error}}</li>
+            @endforeach
+          </ul>
+        @endif
+      </div> --}}
     </div>
   </div>
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
-    {{-- <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button> --}}
     <a href="/jobs" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
   </div>
