d83dbb0592d25e2411b873f0c2f8ed9dc50114c5
AleksoBre <aleksstevancevic@gmail.com>
2026-03-20 13:23:22 +0100

    tidying up create job listings page

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,7 +12,7 @@
           <label for="title" class="block text-sm/6 font-medium text-gray-900">Job Title</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="title" type="text" name="title" placeholder="leatherworker" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+              <input id="title" type="text" name="title" placeholder="leatherworker" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
             </div>
             @error('title')
               <p class="text-sm mt-1 text-red-500 italic font-semibold">{{ $message }}</p>
@@ -24,7 +24,7 @@
           <label for="salary" class="block text-sm/6 font-medium text-gray-900">Job Salary</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="salary" type="text" name="salary" placeholder="$100,000 per year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+              <input id="salary" type="text" name="salary" placeholder="$100,000 per year" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
             </div>
             @error('salary')
               <p class="text-sm mt-1 text-red-500 italic font-semibold">{{ $message }}</p>
@@ -33,16 +33,6 @@
         </div>
   
       </div>
-      
-      {{-- <div class="mt-10">
-        @if ($errors->any())
-          <ul>
-            @foreach ($errors->all() as $error)
-              <li class="text-red-500 italic">{{$error}}</li>
-            @endforeach
-          </ul>
-        @endif
-      </div> --}}
     </div>
   </div>
 
