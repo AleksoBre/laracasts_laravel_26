fd6fa197a9e91a3cd827e64d5d31e91cc4f1c671
AleksoBre <aleksstevancevic@gmail.com>
2026-03-24 16:01:02 +0100

    added routes for auth

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -24,21 +24,16 @@
             <x-form-error id="salary"/>
           </div>
         </x-form-field>
-        
+
       </div>
       
     </div>
   </div>
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
-    <a href="/jobs" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+    <x-form-cancel-button href="/jobs">Cancel</x-form-cancel-button>
+    <x-form-button>Submit</x-form-button>
   </div>
 </form>
-
-
-
-
-
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\JobListingController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -8,3 +9,16 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobListingController::class);
+
+//Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+
+
+
+
+
+
+
+
+
+Route::get('/login', [RegisteredUserController::class, 'show']);
