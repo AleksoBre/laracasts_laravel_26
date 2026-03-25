f18eb593d5f1be60ab2ec163261401e628bbb7df
AleksoBre <aleksstevancevic@gmail.com>
2026-03-25 14:10:28 +0100

    created store method

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,15 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
+    protected $guarded = [];
+
     public function create() {
         return view('auth.register');
     }
 
     public function store() {
+        //validate
+        $attributes = request()->validate([
+            'first_name' => ['required', 'min:3'],
+            'last_name' => ['required', 'min:3'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(5)->symbols(), 'confirmed']
+        ]);
+
+        //create registered user
+        $user = User::create($attributes);
+
+        //log in
+        Auth::login($user);
+
+        //redirect
+        return redirect('/');
+
+
         dd('to do');
     }
 }
